0b7b4fcaa2bfe589ad7b3d68f22b2cac5ef022a2
Abdulrahman Olodeankirun <abdulrahmanolodeankirun@gmail.com>
2026-05-15 19:34:55 +0100

    Final cleanup of components and logic

---
+++ src/services/api.js
@@ -0,0 +1,7 @@
+// src/services/api.js
+
+// The endpoint for the Contact Form submission
+export const CONTACT_API_ENDPOINT = "https://whitebricks.com/tsacademy.php";
+
+// The endpoint for the Planet Data 
+export const PLANET_API_ENDPOINT = "https://anurella.github.io/json/planets.json";
